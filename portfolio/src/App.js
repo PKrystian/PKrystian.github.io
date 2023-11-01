b71f386442e637676273d85c230dce25a3647719
PKrystian <krystian.pinczak2001@gmail.com>
2023-11-01 21:44:54 +0100

    Configured package files for development and added html sample text for testing purpose

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>H1 test</h1>
+            <h2>H2 test</h2>
+            <h3>H3 test</h3>
+            <h4>H4 test</h4>
+            <h5>H5 test</h5>
+            <h6>H6 test</h6>
+            <p>Paragraph test</p>
+            <a href="https://www.google.com">Link test</a>
+            <button>Button test</button>
+            <input type="text" placeholder="Input test" />
+            <label>Label test</label>
+            <select>
+                <option>Option test</option>
+            </select>
+            <textarea>Textarea test</textarea>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Table head test</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Table body test</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Table foot test</td>
+                    </tr>
+                </tfoot>
+            </table>
+            <img src="https://via.placeholder.com/150" alt="Image test" />
+            <iframe src="https://www.google.com" title="Iframe test"></iframe>
+            <div>Div test</div>
+            <span>Span test</span>
+            <ul>
+                <li>Unordered list test</li>
+            </ul>
+            <ol>
+                <li>Ordered list test</li>
+            </ol>
+            <dl>
+                <dt>Definition list term test</dt>
+                <dd>Definition list description test</dd>
+            </dl>
+            <address>Address test</address>
+            <blockquote>Blockquote test</blockquote>
+            <cite>Cite test</cite>
+            <q>Q test</q>
+            <pre>Preformatted text test</pre>
+            <code>Code test</code>
+            <abbr>Abbreviation test</abbr>
+            <b>B test</b>
+            <strong>Strong test</strong>
+            <i>I test</i>
+            <em>Emphasis test</em>
+            <mark>Mark test</mark>
+            <small>Small test</small>
+            <del>Deleted text test</del>
+            <ins>Inserted text test</ins>
+            <sub>Subscript test</sub>
+            <sup>Superscript test</sup>
+        </div>
+    );
 }
 
 export default App;
